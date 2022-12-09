a77eb99d45710957ef84d09b9d392e8be5f75db3
vojtech <p.prusavojta@gmail.com>
2022-12-09 20:31:38 +0100

    DB setup

--- kanban_board/settings.py
+++ kanban_board/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'kanban_board.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+ 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': 'ec2-54-228-218-84.eu-west-1.compute.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
